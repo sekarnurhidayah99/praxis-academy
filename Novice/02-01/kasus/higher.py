834514cadad1a4349f9c9cc75c9f4d038e4184a2
sekarnurhidayah99 <sekarnur1700016091@webmail.uad.ac.id>
2020-03-10 13:33:48 +1400

    uupdatee

--- Novice/02-01/kasus/higher.py
+++ Novice/02-01/kasus/higher.py
@@ -1,6 +1,29 @@
-arr1 = [1, 2, 3]
-arr2 = []
-    for i = 0 
-    return new_numbers
+arr1 =[1,2,3]
+arr2 =[]
 
-print(add2([23, 88])) # [25, 90]
+for i in arr1:
+    arr2.append(i * 2)
+
+print(arr2)
+
+arr1 =[1,2,3]
+arr2 = list(map(lambda arr1 : arr1 * 2, arr1))
+
+print(arr2)
+
+birthYear =[1975,1997, 2002, 1995, 1985]
+
+ages= list(map(lambda birthYear: 2018-birthYear, birthYear))
+
+print(ages)
+
+birthYear =[1975,1997, 2002, 1995, 1985]
+ages =[]
+
+for i in birthYear:
+    ages.append(2018 - birthYear)
+
+print(ages)
+
+
+
---
+++ Novice/02-02/latihan/2.py
@@ -0,0 +1,1 @@
+assert sum([1, 1, 1]) == 6, "Should be 6"
