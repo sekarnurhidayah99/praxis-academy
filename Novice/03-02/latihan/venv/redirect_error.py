5111e4631aa9a92bf87dd0a3dfad991a32df8d67
sekarnurhidayah99 <sekarnur1700016091@webmail.uad.ac.id>
2020-03-22 16:13:50 +1400

    update

---
+++ Novice/03-02/latihan/venv/redirect_error.py
@@ -0,0 +1,10 @@
+from flask import abort, redirect, url_for
+
+@app.route('/')
+def index():
+    return redirect(url_for('login'))
+
+@app.route('/login')
+def login():
+    abort(401)
+    this_is_never_executed()
